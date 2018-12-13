9e4d5dad02243e0e8cee0019253c75e7f6087111
Brian Nguyen <ngxtri@gmail.com>
2018-12-12 22:24:57 -0800

    Add missing android-versions

--- platforms/android/assets/www/app.js
+++ platforms/android/assets/www/app.js
@@ -29,7 +29,8 @@ var bgOptions = {
     startForeground: true,
     stopOnStillActivity: false,
     activityType: 'Other',
-    url: 'http://192.168.3.185:3000/locations',
+    // url: 'http://192.168.168.136/geolocation',//brianserver
+    url: 'http://192.168.1.3/geolocation',//brianmsi
     //syncUrl: 'http://192.168.3.185:3000/sync',
     syncThreshold: 10,
     httpHeaders: {
