6fbeb650c5edc621d31ea5a40ec1a9f5a3e38fe6
brian3t <ngxtri@gmail.com>
2019-01-01 23:35:13 -0800

    prepare app for web only. add mb plugin in place of saralweb plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-saralweb-background-geolocation.backgroundGeolocation",
-    "file": "plugins/cordova-plugin-saralweb-background-geolocation/www/backgroundGeolocation.js",
-    "pluginId": "cordova-plugin-saralweb-background-geolocation",
+    "id": "cordova-plugin-mediabeef-background-geolocation.backgroundGeolocation",
+    "file": "plugins/cordova-plugin-mediabeef-background-geolocation/www/backgroundGeolocation.js",
+    "pluginId": "cordova-plugin-mediabeef-background-geolocation",
     "clobbers": [
       "backgroundGeolocation"
     ]
@@ -13,8 +13,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-saralweb-background-geolocation": "2.4.5",
-  "cordova-android-support-gradle-release": "2.0.1"
+  "cordova-android-support-gradle-release": "2.0.1",
+  "cordova-plugin-mediabeef-background-geolocation": "2.4.6"
 };
 // BOTTOM OF METADATA
 });
--- platforms/android/src/com/saralweb/bgloc/UploadingCallback.java
+++
@@ -1,8 +0,0 @@
-package com.saralweb.bgloc;
-
-/**
- * Created by finch on 20/07/16.
- */
-public interface UploadingCallback {
-    void uploadListener(int progress);
-}
